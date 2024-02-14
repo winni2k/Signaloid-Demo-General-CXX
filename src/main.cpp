528baa0d944481dcb85009cda9afb494073f81ea
Warren Kretzschmar <winni@warrenwk.com>
2024-02-14 12:54:28 +0100

    fix typo

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,11 @@ int main(int argc, char *  argv[])
 
 	double exponential_dist = qexp(1, vec[0]);
 
-	double p_gte_3 = logistic(1, 10, exponential_dist, 3);
+	double p_gt_3 = logistic(1, 10, exponential_dist, 3);
 	double logistic_result = logistic(1, 10, vec[0], 0.5);
 
 	std::cout << exponential_dist << std::endl;
-	std::cout << p_gte_3 << std::endl;
+	std::cout << p_gt_3 << std::endl;
 	std::cout << logistic_result << std::endl;
 
 #ifdef DEBUG
