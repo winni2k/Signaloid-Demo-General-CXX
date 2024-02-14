b2c904eff7abd3e681d576639338aae25c91a259
Warren Kretzschmar <winni@warrenwk.com>
2024-02-14 12:28:01 +0100

    coerce p-val to double

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *  argv[])
 
 	double exponential_dist = qexp(1, vec[0]);
 
-	bool p_lt_3 = exponential_dist < 3;
+	double p_lt_3 = exponential_dist < 3;
 
 	std::cout << exponential_dist << std::endl;
 	std::cout << p_lt_3 << std::endl;
